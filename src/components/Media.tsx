4936681716ac3abc0be51e44ebac436faa499dad
zlxqian1 <101278285+zlxqian1@users.noreply.github.com>
2023-11-26 23:36:14 +0800

    Update Media.tsx

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -4,19 +4,24 @@ import Link from "next/link";
 export default function Media() {
   const mediaList = [
     {
-      title: "代码",
+      title: "代码开源",
       linkText: "Github",
       link: "https://github.com/vectorisvector/inscription",
+    },
+    {
+      title: "推特",
+      linkText: "@kingbtc66",
+      link: "https://twitter.com/kingbtc66",
+    },
+    {
+      title: "电报",
+      linkText: "mint_2025",
+      link: "https://t.me/mint_2025",
     },
     {
       title: "开发者",
       linkText: "@cybervector_",
       link: "https://twitter.com/cybervector_",
-    },
-    {
-      title: "Alpha",
-      linkText: "@ChaunceyCrypto",
-      link: "https://twitter.com/ChaunceyCrypto",
     },
   ];
 
